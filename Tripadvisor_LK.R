3e1e616c6483bb0f7b5bb41ca386225e48e53f64
Lauren Kaapcke <lkaapcke@esm.ucsb.edu>
2018-11-08 11:05:55 -0800

    Added code to find the last page of the results

---
+++ Tripadvisor_LK.R
@@ -0,0 +1,35 @@
+## Tripadvisor Script
+
+## Load libraries
+library(tidyverse)
+library(rvest)
+
+# URL we want to scrape
+url <- 'https://www.tripadvisor.com/Hotels-g34439-Miami_Beach_Florida-Hotels.html'
+
+# Read the HTML code from the site
+webpage <- read_html(url)
+
+# Number of results pages
+last_html <- html_nodes(webpage,'.last')
+
+#Convert to a number
+last_text <- html_text(last_html)
+last_page <- as.numeric(last_text)
+last_page
+
+# Now what data to scrape? Start simple: hotel name, review count, hotel price, rating
+hotel_names_html <- html_nodes(webpage,'.prominent')
+hotel_review_count_html <- html_nodes(webpage, '.review_count')
+hotel_prices_html <- html_nodes(webpage, '.price-wrap .price')
+#hotel_ratings_html <- html_nodes(webpage,'.ui_bubble_rating') Script cannot scrape this because it is a picture
+
+# Now convert to text
+hotel_names <- html_text(hotel_names_html)
+hotel_review_counts <- html_text(hotel_review_count_html)
+hotel_prices <- html_text(hotel_prices_html)
+#hotel_ratings <- html_text(hotel_ratings_html)
+
+# Combine data into a dataframe
+hotel_info_df <- data.frame(HotelName = hotel_names, NumberReviews = hotel_review_counts, Price = hotel_prices)
+hotel_info_df
