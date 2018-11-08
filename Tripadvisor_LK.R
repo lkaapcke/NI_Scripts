bc93739c715abb0a2c3f1d52abd794dcf5a5f179
Lauren Kaapcke <lkaapcke@ucsb.edu>
2018-11-08 14:35:59 -0800

    Removed unnecessary script, JC added loop

--- Tripadvisor_LK.R
+++ Tripadvisor_LK.R
@@ -19,27 +19,13 @@ last_text <- html_text(last_html)
 last_page <- as.numeric(last_text)
 #last_page
 
-# Now what data to scrape? Hotel name, review count, hotel price, rating
-hotel_names_html <- html_nodes(webpage,'.prominent')
-hotel_review_count_html <- html_nodes(webpage, '.review_count')
-hotel_prices_html <- html_nodes(webpage, '.price-wrap .price')
-#hotel_ratings_html <- html_nodes(webpage,'.ui_bubble_rating') Script cannot scrape this because it is a picture
-
-# Now convert to text
-hotel_names <- html_text(hotel_names_html)
-hotel_review_counts <- html_text(hotel_review_count_html)
-hotel_prices <- html_text(hotel_prices_html)
-#hotel_ratings <- html_text(hotel_ratings_html)
-
-# Combine data into a dataframe
-hotel_info_df <- data.frame(HotelName = hotel_names, NumberReviews = hotel_review_counts, Price = hotel_prices)
-hotel_info_df
-
-
-##########################################################
-#Attempting to write a loop for this code:
-
 # Write a loop to find this information for all pages
+
+# Define empty DF
+
+hotel_info_df <- data.frame(HotelName = NA,
+                            NumberReviews = NA,
+                            Price = NA)
 
 for (page in 1:last_page) {
   
@@ -51,24 +37,32 @@ for (page in 1:last_page) {
   
   # Number of results pages
   last_html <- html_nodes(webpage,'.last')
-
+  
   # Now what data to scrape? Hotel name, review count, hotel price, rating
   hotel_names_html <- html_nodes(webpage,'.prominent')
   hotel_review_count_html <- html_nodes(webpage, '.review_count')
   hotel_prices_html <- html_nodes(webpage, '.price-wrap .price')
   #hotel_ratings_html <- html_nodes(webpage,'.ui_bubble_rating') Script cannot scrape this   because it is a picture
-
+  
   # Now convert to text
   hotel_names <- html_text(hotel_names_html)
   hotel_review_counts <- html_text(hotel_review_count_html)
   hotel_prices <- html_text(hotel_prices_html)
+  hotel_prices_full <- c(hotel_prices, rep(NA, length(hotel_names) - length(hotel_prices)))
   #hotel_ratings <- html_text(hotel_ratings_html)
-
+  
   # Combine data into a dataframe
   #hotel_info_df <- data.frame(HotelName = hotel_names, NumberReviews = hotel_review_counts, Price = hotel_prices)
   #hotel_info_df
-
+  
+  hotel_info_df_page <- data.frame(HotelName = hotel_names,
+                                   NumberReviews = hotel_review_counts,
+                                   Price = hotel_prices_full)
+  
+  hotel_info_df <- rbind(hotel_info_df, hotel_info_df_page)
 }
 
-hotel_info_df <- data.frame(HotelName = hotel_names, NumberReviews = hotel_review_counts, Price = hotel_prices)
-hotel_info_df
+hotel_info_df <- drop_na(hotel_info_df)
+View(hotel_info_df)
+
+
