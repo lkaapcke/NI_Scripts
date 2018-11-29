936813e085e3efca1322e7f9bf33409c4f7cdaba
Lauren Kaapcke <lkaapcke@esm.ucsb.edu>
2018-11-29 12:47:29 -0800

    Can now collect URLs, trying to write loop

--- Tripadvisor_URLs_LK.R
+++ Tripadvisor_URLs_LK.R
@@ -6,7 +6,6 @@ library(tidyverse)
 library(rvest)
 library(magrittr)
 library(stringr)
-#library(Rcrawler)
 
 # URL we want to scrape
 url <- 'https://www.tripadvisor.com/Hotels-g34439-oa0-Miami_Beach_Florida-Hotels.html'
@@ -22,49 +21,39 @@ last_text <- html_text(last_html)
 last_page <- as.numeric(last_text)
 last_page
 
-# Try using Rcrawler?
-#Rcrawler(Website = "https://www.tripadvisor.com/Hotels-g34439-Miami_Beach_Florida-Hotels.html", no_cores = 4, no_conn = 4)
+#Read the URLs for each hotel from the page
+urls_html <- webpage %>%
+    html_nodes(".prominent") %>% 
+    html_attr('href')
 
-# Write a loop to find this information for all pages
-
-# Define empty DF
-
-
+# Write a loop to record this information for all pages
+#Define empty DF
 
 hotel_urls_df <- data.frame(Hotel_URL = NA)
 
 for (page in 1:last_page) {
+  
+  # Iterate URL
+  url <- paste("https://www.tripadvisor.com/Hotels-g34439-oa",(page-1)*30,"-Miami_Beach_Florida-Hotels.html", sep ="")
+  
+  # Read code from the site
+  webpage <- read_html(url)
+  
+  # Number of results pages
+  last_html <- html_nodes(webpage,'.last')
+  
+  #Read the URLs for each hotel from the page
+  urls_html <- webpage %>%
+    html_nodes(".prominent") %>% 
+    html_attr('href')
+  
+  hotel_urls_df <- data.frame(Hotel_URL = urls_html)
+  
+  hotel_urls_df_page <- data.frame(Hotel_URL = urls_html)
+  
+  hotel_urls_df <- rbind(hotel_urls_df, hotel_urls_df_page)
+}
 
-# Iterate URL
-url <- paste("https://www.tripadvisor.com/Hotels-g34439-oa",(page-1)*30,"-Miami_Beach_Florida-Hotels.html", sep ="")
+hotel_urls_df <- drop_na(hotel_urls_df)
+View(hotel_urls_df)
 
-# Read the HTML code from the site
-webpage <- read_html(url)
-
-# Number of results pages
-last_html <- html_nodes(webpage,'.last')
-
-# Collect URL
-#hotel_names_html <- html_nodes(webpage,'.prominent')
-#hotel_url_html <- html_nodes(webpage,)
-
-# Now convert to text
-#hotel_names <- html_text(hotel_names_html)
-#hotel_review_counts <- html_text(hotel_review_count_html)
-#hotel_prices <- html_text(hotel_prices_html)
-#hotel_prices_full <- c(hotel_prices, rep(NA, length(hotel_names) - length(hotel_prices)))
-#hotel_ratings <- html_text(hotel_ratings_html)
-
-# Combine data into a dataframe
-#hotel_info_df <- data.frame(HotelName = hotel_names, NumberReviews = hotel_review_counts, Price = hotel_prices)
-#hotel_info_df
-
-#hotel_info_df_page <- data.frame(HotelName = hotel_names,
-#NumberReviews = hotel_review_counts,
-#Price = hotel_prices_full)
-
-#hotel_info_df <- rbind(hotel_info_df, hotel_info_df_page)
-#}
-
-#hotel_info_df <- drop_na(hotel_info_df)
-#View(hotel_info_df)
