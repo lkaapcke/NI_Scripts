fac56ceb1f247ba5c30e887824e55d491b3e67c8
Lauren Kaapcke <lkaapcke@ESM.UCSB.EDU>
2018-11-30 13:02:15 -0800

    I made the loop!!

--- Tripadvisor_URLs_LK.R
+++ Tripadvisor_URLs_LK.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(rvest)
 library(magrittr)
 library(stringr)
+library(rlist)
 
 # URL we want to scrape
 url <- 'https://www.tripadvisor.com/Hotels-g34439-oa0-Miami_Beach_Florida-Hotels.html'
@@ -21,77 +22,38 @@ last_text <- html_text(last_html)
 last_page <- as.numeric(last_text)
 last_page
 
-#Read the URLs for each hotel from the page
-urls_html <- webpage %>%
-    html_nodes(".prominent") %>% 
-    html_attr('href')
-urls_html
-
-# Write a loop to record this information for all pages
-#Define empty DF
-
-hotel_urls_df <- data.frame(Hotel_URL = NA)
+# Create an empty data fram where URLs will be stored
+blank_df <- data.frame(Hotel_URL = NA)
 
 for (page in 1:last_page) {
   
   # Iterate URL
   url <- paste("https://www.tripadvisor.com/Hotels-g34439-oa",(page-1)*30,"-Miami_Beach_Florida-Hotels.html", sep ="")
+  url
   
   # Read code from the site
   webpage <- read_html(url)
   
   # Number of results pages
-  last_html <- html_nodes(webpage,'.last')
-  last_html
+  #last_html <- html_nodes(webpage,'.last')
+  #last_html
   
   #Read the URLs for each hotel from the page
   urls_html <- webpage %>%
     html_nodes(".prominent") %>% 
     html_attr('href')
+  urls_html
   
-  urls_df <- as.data.frame(urls_html)
-  hotel_urls_list[[page]] <- urls_df
+  df_urls <- data.frame(Hotel_URL = urls_html)
+  df_urls
   
-  #hotel_urls_df_page <- data.frame(Hotel_URL = urls_html)
-  
-  #hotel_urls_df <- rbind(hotel_urls_df, hotel_urls_df_page)
+  blank_df <- rbind(blank_df, df_urls)
 }
 
-hotel_urls_df = do.call(rbind,hotel_urls_list)
-View(hotel_urls_df)
+urls_df <- drop_na(blank_df)
+View(urls_df)
 
-#hotel_urls_df <- drop_na(hotel_urls_df)
-#View(hotel_urls_df)
-
-# Trying way on this page: https://stackoverflow.com/questions/29402528/append-data-frames-together-in-a-for-loop
-
-mylist = list()
-
-for(i in 1:last_page) {
-  
-  # Iterate URL
-  url <- paste("https://www.tripadvisor.com/Hotels-g34439-oa",(page-1)*30,"-Miami_Beach_Florida-Hotels.html", sep ="")
-  
-  # Read code from the site
-  webpage <- read_html(url)
-  
-  #Read the URLs for each hotel from the page
-  crawl <- webpage %>%
-    html_nodes(".prominent") %>% 
-    html_attr('href')
-  
-  crawl <- data.frame(URL = crawl)
-  #View(crawl)
-  crawl$i <- i
-  link_list[[i]] <- crawl
-  View(link_list)
-  
-}
-
-links = do.call(cbind, crawl)
-View(links)
- 
-
+# To do: concatenate every row with the beginning of the url
 
 
 
