ab942c62846f9548f717762ce9a202d138c6198c
Lauren Kaapcke <lkaapcke@ESM.UCSB.EDU>
2018-12-16 15:54:33 -0800

    Made changes due to interface changing

--- Tripadvisor_LK.R
+++ Tripadvisor_LK.R
@@ -20,11 +20,11 @@ name <- html_text(html_nodes(hotel_url, '#HEADING'))
 address_info <- html_text(html_nodes(hotel_url, '.public-business-listing-ContactInfo__nonWebLink--1EqMn span'))
 address <- address_info[1]
 
-# Pull hotel address
-rating <- str_trim(html_text(html_nodes(hotel_url,'.hotels-hotel-review-about-with-photos-Reviews__overallRating--3cjYf')), side = "both")
+# Pull hotel rating
+rating <- as.numeric(str_trim(html_text(html_nodes(hotel_url,'.overallRating')), side = "both"))
 
 # Scrape info from bottom of page to mine for prices and number of rooms
-more_info <- html_text(html_nodes(hotel_url, '.hotels-hotel-review-layout-Section__plain--1HV0a'))
+more_info <- html_text(html_nodes(hotel_url, '.ui_section'))
 
 # Extract prices
 price_low <- as.numeric(str_extract(str = more_info, pattern = "[:digit:]+"))
@@ -41,11 +41,6 @@ stars <- as.character(html_nodes(hotel_url, '.is-6+ .is-6 .hotels-hotel-review-a
 # Extract hotel class
 hotel_class <- paste(substr(str_extract(stars, pattern = "_[:digit:]+"), 2, 2), ".", substr(str_extract(stars, pattern = "_[:digit:]+"), 3, 3), sep = "")
 
-#Scrape for amenities Interface 1 (with show more button)
-amenities <- html_text(html_nodes, '.hotels-hotel-review-about-with-photos-layout-Group__group--KGq7E:nth-child(1) .hotels-hotel-review-about-with-photos-AmenityGroup__amenitiesList--1kgjY')
-
-
-
 # Collect all the scraped data into a df
 
 hotel_scrape <- data.frame(Hotel_Name = name,
@@ -54,24 +49,10 @@ hotel_scrape <- data.frame(Hotel_Name = name,
                           Low_Price = price_low,
                           High_Price = price_high,
                           Number_Rooms = num_rooms,
-                          Hotel_Class = hotel_class,
-                          Amenities_1 = amenities)
+                          Hotel_Class = hotel_class,)
 
 View(hotel_scrape)
 
 # To Do:
-# Scrape amenities
-# Separate amenities into columns
 # Put everthing into a df
 # Create loop to do this to every URL in a different df
-
-################### Below: broken amenities scraping (old TA interface)
-
-#amenities <- html_text(html_nodes(hotel_url,'.is-6-desktop .textitem'))
-#amenities <- html_text(html_nodes(hotel_url, ".hotels-hotel-review-about-with-photos-layout-Group__group--KGq7E:nth-child(1) .hotels-hotel-review-about-with-photos-AmenityGroup__amenitiesList--1kgjY"))
-#amenities
-#amenities <- html_text(html_nodes(hotel_url, '.hotels-hotel-review-about-with-photos-AmenitiesModal__group--1PHad'))
-#amenities
-
-#unavail_amenities <- html_text(html_nodes(hotel_url,'.is-6-desktop .unavailable'))
-#unavail_amenities
